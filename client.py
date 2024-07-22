aded93d06cbbe247d876dd992f9562120d10754c
luis <luis_jna@outlook.com>
2024-07-22 11:16:49 -0300

    adicao de pacote vazio marcador de fim de envio

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 
+BUFFER_SIZE = 1024
 
 # Cria um socket UDP
 socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -8,24 +9,14 @@ socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 endereco_servidor = ("LocalHost", 12345)
 
 while True:
-
-	# Mensagem a ser enviada ao servidor
-	message = input("Digite a mensagem:")
-	# print(f"Enviando: {message}")
-	socket_udp.sendto(message.encode(), endereco_servidor)
-	with open("Bear.mp4", "wb") as arquivo:
-		while True:
-			data, addr = socket_udp.recvfrom(10000)  # 1024 é o buffer size
-			print(data)
-			if not data:
-				break
-			arquivo.write(data)
-		
-
-    # Recebe a mensagem
-    # data, addr = socket_udp.recvfrom(1024)  # 1024 é o buffer size
-    # data = data.decode()
-    # print(f"Recebido de {addr}: {data}")
-    # print(socket.gethostbyaddr())
-      
-
+    # Mensagem a ser enviada ao servidor
+    message = input("Digite a mensagem:")
+    socket_udp.sendto(message.encode(), endereco_servidor)
+    if message == "envia":
+        with open("Bear.mp4", "wb") as arquivo:
+            while True:
+                data, addr = socket_udp.recvfrom(BUFFER_SIZE)  # 1024 é o buffer size
+                if not data.strip(b'\x00'):
+                    break
+                arquivo.write(data)
+        print("arquivo recebido e salvo como Bear.mp4")
