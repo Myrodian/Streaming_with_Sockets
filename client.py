532e0d7ecec5f7a02d8d9f76d6db7b85c32a0a5a
luis <luis_jna@outlook.com>
2024-07-23 19:40:20 -0300

    teste de controle

--- client.py
+++ client.py
@@ -1,39 +1,53 @@
 import socket
 import subprocess
+
 BUFFER_SIZE = 1024
-caminho_vlc = 'D:\\Arquivos_e_Programas\\VLC\\vlc.exe'
+SHIPPING_SIZE = 10
+
+caminho_vlc = 'C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe'
+
 # Cria um socket UDP
 socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 # Endereço e porta do servidor
 endereco_servidor = ("LocalHost", 12345)
-envia_video = subprocess.Popen([caminho_vlc,'-', '--input-title-format', "Streaming Video"],stdin = subprocess.PIPE,)
+
+
 cont = 0
-controle = True
-i = 0
-def trasmissao():
-    while controle:
-            controle = False
-            mensagem, addr = socket_udp.recvfrom(BUFFER_SIZE)
+
+def fim_arquivo(data: bytes):
+    if data.strip(b'\x00'):
+        if data == b'EOF':
+            return True
+    return False
+
+def transmite(cont):
+    i = 0
+    while True:
+            
+            data, addr = socket_udp.recvfrom(BUFFER_SIZE) # novos dados
+
+            if fim_arquivo(data): # caso de arquivo vazio
+                break
+
             i += 1
-            if i == 10:
-                controle = True
-                socket_udp.sendto(b'1', endereco_servidor)
-                data, addr = socket_udp.recvfrom(BUFFER_SIZE)
-                if data.strip(b'\x00'):
-                    if data == b'EOF':
-                        break
-                envia_video.stdin.write(data)
-                cont += 1
-                print(f"pacote {cont}")
 
+            if i == SHIPPING_SIZE: # controle de entrega
+                i = 0 # se prepara para nova remessa
+                socket_udp.sendto(b'1', endereco_servidor) # avisa que pode receber mais
+                    
+            envia_video.stdin.write(data) # salvando novos dados
 
+            cont += 1
+            print(f"pacote {cont}")
 
 while True:
     # Mensagem a ser enviada ao servidor
     message = input("Digite a mensagem:")
     socket_udp.sendto(message.encode(), endereco_servidor)
     if message == "envia":
-        trasmissao()
+        envia_video = subprocess.Popen([caminho_vlc,'-', '--input-title-format', "Streaming Video"],stdin = subprocess.PIPE,)
+        transmite(cont) # trocar nome
         
     print(f"foram necessarios {cont} pacotes para mandar todo o arquivo")
        
