119054d8602826b3fe0bc45ee5a23f6e8277397e
luis <luis_jna@outlook.com>
2024-07-26 21:16:51 -0300

    ajuste de write

--- client.py
+++ client.py
@@ -19,26 +19,36 @@ def fim_arquivo(data: bytes):
 
 def pedido():
     cont = 0
+
+    # verificar se cabe um video
     i = 0
-    # vid_buff = bytearray(b'')
+    # vid_buff = b''
     vid_buff = []
     while True:
         try:
             data, addr = socket_udp.recvfrom(BUFFER_SIZE)  # novos dados
-            
+            print(f"\rPacote {cont} recebido!", end='')
               
             if fim_arquivo(data):  # caso de arquivo vazio
                 break
             
             vid_buff.append(data)
             # print(vid_buff)
+            
+            
+            
+            # calcular bite rate
+
 
 
             # tem que passar isso para uma thread
-            if cont > 2000:
-                while i != ((len(vid_buff))-1):
-                    envia_video.stdin.write(vid_buff[i]) # salvando novos dados
-                    i += 1
+            if (cont % 1000 == 0) and cont > 0: # 1000 pacotes de 1024 bytes da 1.024.000/4096 = 500 writes
+                
+                while (i + 4096) <= ((len(vid_buff)) - 1):
+                    pedaco = b''.join(vid_buff[i:i+4096])
+                    envia_video.stdin.write(pedaco) # salvando novos dados
+                    i += 4096
+                # reiniciar buffer caso cheio
             
             # vid_buff.append(data)
             # if i != ((len(vid_buff))-50):
@@ -47,9 +57,9 @@ def pedido():
             #         i += 1
             #     # i= 0
             socket_udp.sendto(b'1', addr)  # avisa que pode receber mais
-            
             cont += 1
-            print(f"\rPacote {cont} recebido!", end='')
+
+            
 
         except BrokenPipeError:
             print("\nErro: Broken pipe. O VLC fechou a conexão.")
@@ -57,7 +67,8 @@ def pedido():
         except Exception as e:
             print(f"\nErro inesperado: {e}")
             break
-
+    
+    # resetar buff
     return cont
 
 while True:
