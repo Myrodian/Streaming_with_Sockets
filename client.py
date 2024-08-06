753c224b1a55d75d9a8f7643db52ae471b46e258
unknown <augustoleal11@gmail.com>
2024-08-06 14:28:51 -0300

    ta funcionando as conexão

--- client.py
+++ client.py
@@ -1,19 +1,27 @@
 import socket
 import subprocess
 
-VIDEO_LIST_SIZE = 3
+VIDEO_LIST_SIZE = 4
 BUFFER_SIZE = 1024 * 2
-WRITE_BUFF = 1024 * 4
-caminho_vlc = 'C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe'
-# caminho_vlc = 'D:\\Arquivos_e_Programas\\VLC\\vlc.exe'
-# Cria um socket UDP
-socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server = 'localhost'
+# caminho_vlc = 'C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe'
+caminho_vlc = 'D:\\Arquivos_e_Programas\\VLC\\vlc.exe'
+try:
+    socket_UDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Cria um socket UDP
+    socket_TCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Cria um socket TCP
 
-# Endereço e porta do servidor
-server_addr = ("localhost", 12345)
+    # Endereço e porta do servidor
+    addr_server_UDP = (server, 12345)  # Endereço e porta do servidor UDP
+    addr_server_TCP = (server, 54321)  # Endereço e porta do servidor TCP
+    
+    socket_TCP.connect(addr_server_TCP)
+    socket_TCP.sendall(b'Ola servidor, estoy usando o canal TCP')
+
+except Exception as e:
+      print(f"\nErro inesperado: {e}")
 
 def ready(addr):
-            socket_udp.sendto(b'1', addr)  # Avisa que pode receber mais
+            socket_UDP.sendto(b'1', addr)  # Avisa que pode receber mais
 
 def request():
     vid_buff = []
@@ -22,8 +30,9 @@ def request():
                                     stdin=subprocess.PIPE)
     vezes = 0
     try:
+
         while True:
-            data, addr = socket_udp.recvfrom(BUFFER_SIZE)  # Novos dados
+            data, addr = socket_UDP.recvfrom(BUFFER_SIZE)  # Novos dados
             vezes += 1
             if data == b'EOF':  # Caso de arquivo vazio
 
@@ -35,37 +44,37 @@ def request():
 
             # Processa e escreve dados no VLC conforme são recebidos
             while vid_buff:
-                slice = b''.join(vid_buff)
-                envia_video.stdin.write(slice)
+                piece = b''.join(vid_buff)
+                envia_video.stdin.write(piece)
                 envia_video.stdin.flush()  # Garante que os dados sejam enviados ao VLC
                 vid_buff.clear()  # Limpa o buffer depois de escrever
 
             # ready(addr)
-            print(f"\rPacote {vezes} recebido!", end="")
+            # print(f"\rPacote {vezes} recebido!", end="")
     except BrokenPipeError:
         print("\nErro: Broken pipe. O VLC fechou a conexão.")
     except Exception as e:
         print(f"\nErro inesperado: {e}")
+    
+    return vezes
+if __name__ == "__main__":
+    while True:
+        message = input("Escolha o video [ 1 - BBB | 2 - Bear | 3 - WildLife ]:")
+        if message == "0":
+            break
+        if int(message) in range(VIDEO_LIST_SIZE):
+            socket_UDP.sendto(message.encode(), addr_server_UDP)
 
-    return len(vid_buff)
+            try:
+                cont = request()
 
-while True:
-    message = input("Escolha o video [ 0 - BBB | 1 - Bear | 2 - WildLife ]:")
-    if message == "-1":
-        break
-    if int(message) in range(VIDEO_LIST_SIZE):
-        
-        socket_udp.sendto(message.encode(), server_addr)
+                # ready(server_addr)
 
-        try:
-            cont = request()
+            except Exception as e:
+                print(f"Erro ao iniciar o VLC: {e}")
+        else: 
+            print(f"Valor {message} inválido! Tente novamente.")
 
-            # ready(server_addr)
-
-        except Exception as e:
-            print(f"Erro ao iniciar o VLC: {e}")
-    else: 
-        print(f"Valor {message} inválido! Tente novamente.")
-
-    print(f"\nForam necessários {cont} pacotes para mandar todo o arquivo")
-socket_udp.close()
+        print(f"\nForam necessários {cont} pacotes para mandar todo o arquivo")
+    socket_UDP.close()
+    socket_TCP.close()
--- server.py
+++ server.py
@@ -3,90 +3,112 @@ import os
 import time
 import subprocess
 import json
+import threading
 
 BUFFER_SIZE = 1024
-VID_ARR_SIZE = 3
-EXIT = -1
-WAIT = -2
-video_array = ["./Conteudo/BigBuckBunny.mp4","./Conteudo/Bear.mp4","./Conteudo/Wildlife.mp4"]
+server = 'localhost'
+
+video_array = ["./Conteudo/BigBuckBunny.mp4", "./Conteudo/Bear.mp4", "./Conteudo/Wildlife.mp4"]
 
 def get_vid_time(vid_path) -> int:
     result = subprocess.run(['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'json', vid_path],
-        stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
-    )
-
+                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
     output = json.loads(result.stdout)
-
     duration = float(output['format']['duration'])
-
     return int(duration)
 
 def get_bit_rate(vid_path):
-    byte_por_segundo = int(os.path.getsize(vid_path)/get_vid_time(vid_path)) # byte / seg
-    return byte_por_segundo 
+    byte_por_segundo = int(os.path.getsize(vid_path) / get_vid_time(vid_path))  # byte / seg
+    return byte_por_segundo
 
 def wait_message():
-    
     try:
-        message, addr = socket_udp.recvfrom(BUFFER_SIZE)
+        message, addr = socket_UDP.recvfrom(BUFFER_SIZE)
         message = message.decode()
         return message, addr
     except socket.error as e:
-        print(f"Erro ao receber mensagem: {e}")
+        print(f"Erro ao receber mensagem UDP: {e}")
+        return None, None
 
-try:
-    # message = b''
-    socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Cria um socket UDP
-    endereco_servidor = ('localhost', 12345)  # Endereço e porta do servidor
-    socket_udp.bind(endereco_servidor)  # Liga o socket ao endereço e porta
+def handle_client_connection(client_socket):
+    try:
+        while True:
+            command = client_socket.recv(BUFFER_SIZE)
+            if not command:
+                break
+            print(f"Comando {command.decode()} recebido!")
+            if command == b'0':
+                break
+    except socket.error as e:
+        print(f"Erro ao receber comando TCP: {e}")
+    finally:
+        client_socket.close()
 
-    print(f"Servidor UDP está rodando na porta {endereco_servidor[1]}")
-    print("Aguardando mensagens...")
+if __name__ == "__main__":
+    try:
+        # Cria sockets UDP e TCP
+        socket_UDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        socket_TCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    while True:
+        # Define endereços e portas separadamente para UDP e TCP
+        addr_server_UDP = (server, 12345)
+        addr_server_TCP = (server, 54321)
 
-        print("esperando por novos pedidos :)")
-    
-        message, addr = wait_message()
+        # Liga cada socket ao seu respectivo endereço e porta
+        socket_UDP.bind(addr_server_UDP)
+        socket_TCP.bind(addr_server_TCP)
+        socket_TCP.listen(5)
 
-        # Pega tamanho do vídeo escolhido
-        try:
-            index = int(message)
-            byte_rate = get_bit_rate(video_array[index])
-            print(f"Para o video {video_array[index]}, a vazão necessaria de {byte_rate} bits por segundo")
-        except FileNotFoundError:
-            print(f"Erro: Arquivo '{video_array[index]}' não encontrado.")
-        except OSError as e:
-            print(f"Erro ao acessar o arquivo: {e}")
-            
-        with open(video_array[index], "rb") as arquivo:
-            vezes = 0
+        print(f"Servidor está rodando {addr_server_UDP[0]} na porta UDP {addr_server_UDP[1]} e na porta TCP {addr_server_TCP[1]}")
+
+        def start_command_thread():
             while True:
-                # Bites devem ser compostos por: bytes de vídeo
-                bytes = arquivo.read(BUFFER_SIZE)
-                if not bytes:
-                    socket_udp.sendto(b'EOF', addr)  # Marcador de fim de pacote
-                    break
+                client_socket, addr = socket_TCP.accept()
+                print(f"Cliente TCP conectado: {addr}")
+                client_thread = threading.Thread(target=handle_client_connection, args=(client_socket,))
+                client_thread.start()
 
-                socket_udp.sendto(bytes, addr) 
-                vezes += 1                       
-                print(f'\rPacote {vezes} enviado!', end='')
+        # Inicia a thread para lidar com comandos TCP
+        command_thread = threading.Thread(target=start_command_thread)
+        command_thread.start()
 
-                # Calcular o tempo de espera
-                time_to_wait = len(bytes)/byte_rate  # tempo em segundos
-                # print(f"\rtime_to_wait ={time_to_wait}", end=)
-                time.sleep(time_to_wait)  # Pausa para controlar a vazão
+        while True:
+            print("Esperando por novos pedidos UDP :)")
+            message, addr = wait_message()
 
-            print("\nArquivo enviado!")
+            if message is None:
+                continue
 
-            # espera cliente fechar vlc
-            
+            # Pega tamanho do vídeo escolhido
+            try:
+                index = int(message)
+                index = index - 1
+                byte_rate = get_bit_rate(video_array[index])
+                print(f"Para o vídeo {video_array[index]}, a vazão necessária é de {byte_rate} bits por segundo")
+            except FileNotFoundError:
+                print(f"Erro: Arquivo '{video_array[index]}' não encontrado.")
+            except OSError as e:
+                print(f"Erro ao acessar o arquivo: {e}")
 
+            with open(video_array[index], "rb") as arquivo:
+                vezes = 0
+                while True:
+                    bytes = arquivo.read(BUFFER_SIZE)
+                    if not bytes:
+                        socket_UDP.sendto(b'EOF', addr)  # Marcador de fim de pacote
+                        break
 
+                    socket_UDP.sendto(bytes, addr)
+                    vezes += 1
 
-except socket.error as e:
-    print(f"Erro ao criar ou ligar o socket: {e}")
+                    # Calcular o tempo de espera
+                    time_to_wait = len(bytes) / byte_rate
+                    time.sleep(time_to_wait)  # Pausa para controlar a vazão
+                print("\nArquivo enviado!")
 
-finally:
-    socket_udp.close()
-    print("Socket fechado")
+    except socket.error as e:
+        print(f"Erro ao criar ou ligar o socket: {e}")
+    finally:
+        socket_UDP.close()
+        socket_TCP.close()
+        print("Sockets fechados")
